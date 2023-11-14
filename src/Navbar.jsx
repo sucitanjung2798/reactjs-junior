fcf66446e7f884cc784c37c2d3a978bf7014c410
sucitanjung2798 <sucitanjung2798@gmail.com>
2023-11-15 03:06:03 +0700

    add css

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom";
+
 const Navbar = () => {
   return (
     <div style={{ boxShadow: "0 10px 15px -3px rgb(0 0 0 / 0.1)" }}>
       <div className="bg-white text-black p-4 flex gap-4 justify-between items-center container mx-auto px-8">
         <div className="flex gap-8 items-center">
-          <h1 className="text-2xl font-bold">Uhuyy Movie</h1>
+          <Link to="/" className="text-2xl font-bold text-black title-page">
+            Uhuyy Movie
+          </Link>
           <div className="flex items-center">
             <input
               type="text"
